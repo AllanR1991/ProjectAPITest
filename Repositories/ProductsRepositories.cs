1717d23d28f1576468727dc16a54f9192cc886eb
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-08-05 10:56:22 -0300

    Criado Repositories

---
+++ Repositories/ProductsRepositories.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectAPITest.Context;
+using ProjectAPITest.Domains;
+using ProjectAPITest.Interface;
+
+namespace ProjectAPITest.Repositories
+{
+    public class ProductsRepositories : IProductsRepository
+    {
+        ProjectApiContextContext _context = new ProjectApiContextContext();
+        public void Delete(Guid id)
+        {
+            Products? productsFind = _context.Products.FirstOrDefault(product => product.IdProduct == id);
+            
+            if (productsFind != null)
+            {
+                _context.Products.Remove(productsFind);
+            }
+
+            _context.SaveChanges();
+        }
+
+        public List<Products> Get()
+        {
+            return _context.Products.ToList();
+        }
+
+        public Products? GetById(Guid id)
+        {
+            return _context.Products.FirstOrDefault(produto => produto.IdProduct == id);            
+        }
+
+        public Products Post(Products product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Products Put(Guid id, Products product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
