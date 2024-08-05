1723e13edf29e611013876133e6c65778e6ed992
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-08-05 10:56:05 -0300

    Migrations realizada com sucesso.

---
+++ Migrations/20240805124732_Initial.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ProjectAPITest.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    IdProduct = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "VARCHAR(100)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.IdProduct);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "IdProduct", "Name", "Price" },
+                values: new object[,]
+                {
+                    { new Guid("3a6c1b48-00a1-4c3b-96af-33d7025a967b"), "Apple iPhone 15 Pro Max 512 GB -Titânio Natural", 9699m },
+                    { new Guid("f1146f04-4a03-4b9b-a9c4-38aa118d8308"), "Samsung A52s", 2768.90m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
