1d0683de96bdfe8ae281a63fbe6425b4af0c10a8
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-08-05 10:55:39 -0300

    Criado Domains

---
+++ Domains/Products.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectAPITest.Domains
+{
+    public class Products
+    {
+        /// <summary>
+        /// Identificador único do produto.
+        /// </summary>
+        /// <remarks>
+        /// Esta propriedade é a chave primária da entidade e é gerada automaticamente pelo banco de dados.
+        /// </remarks>
+        [Key] // Marca esta propriedade como a chave primária da entidade.
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Indica que o valor é gerado pelo banco de dados (identidade).
+        public Guid IdProduct { get; set; }
+
+        /// <summary>
+        /// Nome do produto.
+        /// </summary>
+        /// <remarks>
+        /// Esta propriedade é armazenada como uma string com comprimento máximo de 100 caracteres.
+        /// </remarks>
+        [Column(TypeName = "VARCHAR(100)")] // Define o tipo de coluna no banco de dados como VARCHAR com comprimento máximo de 100 caracteres.
+        [Required("Nome do Produto é Obrigatório.")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Preço do produto.
+        /// </summary>
+        /// <remarks>
+        /// Esta propriedade é armazenada como um decimal com até 18 dígitos no total e 2 dígitos após o ponto decimal.
+        /// </remarks>
+        [Column(TypeName = "decimal(18,2)")] // Define o tipo de coluna no banco de dados como decimal com precisão 18 e escala 2.
+        [Required("Preço do produto é obrigatório.")]
+        public decimal Price { get; set; }
+    }
+}
